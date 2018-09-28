4d8cff2984dc5c26542cdf39ac4bb502fc3acc0c
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-09-28 08:48:11 +0300

    add class handle

--- main_gui.py
+++ main_gui.py
@@ -72,101 +72,131 @@ class SystemDoors(tkinter.Frame):
         self.photo = ImageTk.PhotoImage(self.image)
         self.label = tkinter.Label(self, image=self.photo)
         self.label.pack()
-        self.button_chage = tkinter.Button(self,
-                                           text="Выбрать систему",
-                                           command=lambda: SystemDoorsChange(self))
-        self.button_chage.pack(pady=4)
 
     def refresh(self):
         self.label.destroy()
-        self.button_chage.destroy()
         self.make_widget()
 
 
 class ParametersDoorOpening(tkinter.Frame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.pack(fill='x')
-        self.label = tkinter.Label(self, text="1. Введите параметры проёма и дверей-купе: ", bg='#1ad924', width=50)
-        self.label.pack()
-        self.row_frame = []
-        self.fields = [
-            'Высота проёма:',
-            'Ширина проёма:',
-            'Количество дверей:',
-            'Количество мест перекрытия:',
-            'Буферная лента ("шлегель"):'
-        ]
-        self.amount_doors = tkinter.StringVar()
-        self.amount_doors.set(1)  # default value
-        self.amount_opening = tkinter.StringVar()
-        self.amount_opening.set(1)  # default value
-        self.need_tape = tkinter.IntVar()
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.pack(fill='x')
+                self.label = tkinter.Label(self, text="1. Введите параметры проёма и дверей-купе: ", bg='#1ad924', width=50)
+                self.label.pack()
+                self.row_frame = []
+                self.fields = [
+                    'Высота проёма:',
+                    'Ширина проёма:',
+                    'Количество дверей:',
+                    'Количество мест перекрытия:',
+                    'Буферная лента ("шлегель"):'
+                ]
+                self.amount_doors = tkinter.StringVar()
+                self.amount_doors.set(1)  # default value
+                self.amount_opening = tkinter.StringVar()
+                self.amount_opening.set(1)  # default value
+                self.need_tape = tkinter.IntVar()
 
-        #  'Высота проёма:'
-        row = tkinter.Frame(self, relief='ridge', bd=1)
-        row.pack(side='top', fill='x')
-        self.height = tkinter.Entry(row)
-        self.height.insert(0, '2500')
-        mes = tkinter.Message(row, text=self.fields[0], width=100)
-        mes.pack(side='left')
-        self.height.pack(side="right", pady=2)
-        self.row_frame.append(row)
+                #  'Высота проёма:'
+                row = tkinter.Frame(self, relief='ridge', bd=1)
+                row.pack(side='top', fill='x')
+                self.height = tkinter.Entry(row)
+                self.height.insert(0, '2500')
+                mes = tkinter.Message(row, text=self.fields[0], width=100)
+                mes.pack(side='left')
+                self.height.pack(side="right", pady=2)
+                self.row_frame.append(row)
 
-        #  'Ширина проёма:'
-        row = tkinter.Frame(self, relief='ridge', bd=1)
-        row.pack(side='top', fill='x')
-        self.width = tkinter.Entry(row)
-        self.width.insert(0, '1500')
-        mes = tkinter.Message(row, text=self.fields[1], width=100)
-        mes.pack(side='left')
-        self.width.pack(side="right", pady=2)
-        self.row_frame.append(row)
+                #  'Ширина проёма:'
+                row = tkinter.Frame(self, relief='ridge', bd=1)
+                row.pack(side='top', fill='x')
+                self.width = tkinter.Entry(row)
+                self.width.insert(0, '1500')
+                mes = tkinter.Message(row, text=self.fields[1], width=100)
+                mes.pack(side='left')
+                self.width.pack(side="right", pady=2)
+                self.row_frame.append(row)
 
-        #  'Количество дверей:'
-        row = tkinter.Frame(self, relief='ridge', bd=1)
-        row.pack(side='top', fill='x')
-        list_num = [1, 2, 3, 4, 5, 6, 7, 8]
-        option = tkinter.OptionMenu(row, self.amount_doors, *list_num)
-        mes = tkinter.Message(row, text=self.fields[2], width=150)
-        mes.pack(side='left')
-        option.pack(side='right')
-        self.row_frame.append(row)
+                #  'Количество дверей:'
+                row = tkinter.Frame(self, relief='ridge', bd=1)
+                row.pack(side='top', fill='x')
+                list_num = [1, 2, 3, 4, 5, 6, 7, 8]
+                option = tkinter.OptionMenu(row, self.amount_doors, *list_num)
+                mes = tkinter.Message(row, text=self.fields[2], width=150)
+                mes.pack(side='left')
+                option.pack(side='right')
+                self.row_frame.append(row)
 
-        #  'Количество мест перекрытия:'
-        row = tkinter.Frame(self, relief='ridge', bd=1)
-        row.pack(side='top', fill='x')
-        list_num = [1, 2, 3, 4, 5, 6, 7]
-        option = tkinter.OptionMenu(row, self.amount_opening, *list_num)
-        mes = tkinter.Message(row, text=self.fields[3], width=200)
-        mes.pack(side='left')
-        option.pack(side='right')
-        self.row_frame.append(row)
+                #  'Количество мест перекрытия:'
+                row = tkinter.Frame(self, relief='ridge', bd=1)
+                row.pack(side='top', fill='x')
+                list_num = [1, 2, 3, 4, 5, 6, 7]
+                option = tkinter.OptionMenu(row, self.amount_opening, *list_num)
+                mes = tkinter.Message(row, text=self.fields[3], width=200)
+                mes.pack(side='left')
+                option.pack(side='right')
+                self.row_frame.append(row)
 
-        #  'Буферная лента ("шлегель"):'
-        row = tkinter.Frame(self)
-        row.pack(side='top', fill='x')
-        check1 = tkinter.Checkbutton(row, variable=self.need_tape, onvalue=1, offvalue=0)
-        mes = tkinter.Message(row, text=self.fields[4], width=200)
-        self.image = Image.open(f'{settings.data}/quest.png')
-        self.image = self.image.resize((15, 15), Image.ANTIALIAS)
-        self.photo = ImageTk.PhotoImage(self.image)
-        button_quest = tkinter.Button(
-            row,
-            relief='groove',
-            image=self.photo,
-            command=lambda _name='quest_tape': print(_name))
+                #  'Буферная лента ("шлегель"):'
+                row = tkinter.Frame(self)
+                row.pack(side='top', fill='x')
+                check1 = tkinter.Checkbutton(row, variable=self.need_tape, onvalue=1, offvalue=0)
+                mes = tkinter.Message(row, text=self.fields[4], width=200)
+                self.image = Image.open(f'{settings.data}/quest.png')
+                self.image = self.image.resize((15, 15), Image.ANTIALIAS)
+                self.photo = ImageTk.PhotoImage(self.image)
+                button_quest = tkinter.Button(
+                    row,
+                    relief='groove',
+                    image=self.photo,
+                    command=lambda _name='quest_tape': print(_name))
 
-        mes.pack(side='left')
-        button_quest.pack(side='left')
-        check1.pack(side="right", pady=2)
+                mes.pack(side='left')
+                button_quest.pack(side='left')
+                check1.pack(side="right", pady=2)
 
-        self.row_frame.append(row)
+                self.row_frame.append(row)
 
 
 class DoorHandle(tkinter.Frame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.pack(fill='x')
+        self.label = tkinter.Label(self, text="2. Выберите профиль вертикальной ручки: ", bg='#1ad924', width=50)
+        self.label.pack()
+        self.row_frame = []
+        self.fields = [
+            'Профиль Absolut: ',
+            'Цвет профиля Absolut: ',
+        ]
+        self.type_handle = tkinter.StringVar()
+        self.type_handle.set(1)  # default value
+        self.color_handle = tkinter.StringVar()
+        self.color_handle.set(1)  # default value
+        self.need_tape = tkinter.IntVar()
+
+        #  Профиль
+        row = tkinter.Frame(self, relief='ridge', bd=1)
+        row.pack(side='top', fill='x')
+        list_num = [1, 2, 3, 4, 5, 6, 7, 8]
+        option = tkinter.OptionMenu(row, self.type_handle, *list_num)
+        mes = tkinter.Message(row, text=self.fields[0], width=150)
+        mes.pack(side='left')
+        option.pack(side='right')
+        self.row_frame.append(row)
+
+        #  Цвет профиля
+        row = tkinter.Frame(self, relief='ridge', bd=1)
+        row.pack(side='top', fill='x')
+        list_num = [1, 2, 3, 4, 5, 6, 7]
+        option = tkinter.OptionMenu(row, self.color_handle, *list_num)
+        mes = tkinter.Message(row, text=self.fields[1], width=200)
+        mes.pack(side='left')
+        option.pack(side='right')
+        self.row_frame.append(row)
+
+        self.row_frame.append(row)
 
 
 class MainFrame(tkinter.Frame):
@@ -176,6 +206,10 @@ class MainFrame(tkinter.Frame):
         self.sys_door = SystemDoors(self)
         self.param_door = ParametersDoorOpening(self)
         self.door_handle = DoorHandle(self)
+        self.button_chage = tkinter.Button(self,
+                                           text="Выбрать систему",
+                                           command=lambda: SystemDoorsChange(self.sys_door))
+        self.button_chage.pack(pady=4)
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
 
 system_doors_path_img = 'system_doors/img_system_doors'
+handles = 'system_doors/vertical_handle_profile'
 data = 'data'
+
