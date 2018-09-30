b90588cdf16583899e5bca4100dd7a0c9074a525
rvfedorin <35657347+rvfedorin@users.noreply.github.com>
2018-09-30 11:19:43 +0300

    added change materials in parts of door

--- form_view_classes.py
+++ form_view_classes.py
@@ -26,8 +26,28 @@ class FormNoSection:
         self.door = self.canvas.create_rectangle(50, 20, 190, 240)
         self.part_left = self.canvas.create_rectangle(56, 26, 117, 234)
         self.part_right = self.canvas.create_rectangle(123, 26, 184, 234)
-        self.part_left_mat = self.canvas.create_image(86.5, 130, image=self.mirror)
-        self.part_right_mat = self.canvas.create_image(153.5, 130, image=self.ldsp)
+
+        self.mat_dict = {'left': self.mirror, 'right': self.ldsp}
+
+        self.part_left_mat = self.canvas.create_image(86.5, 130, image=self.mat_dict['left'])
+        self.part_right_mat = self.canvas.create_image(153.5, 130, image=self.mat_dict['right'])
+
+        self.parts = {
+            'left': self.part_left_mat,
+            'right': self.part_right_mat
+        }
+
+        self.canvas.tag_bind(self.part_left_mat, '<Button-1>', lambda event: self.change_material(event, 'left'))
+        self.canvas.tag_bind(self.part_right_mat, '<Button-1>', lambda event: self.change_material(event, 'right'))
+
+    def change_material(self, event, change_side):
+
+        for i in self.parts.keys():
+            if i == change_side:
+                new_mat = self.mirror if self.mat_dict[change_side] == self.ldsp else self.ldsp
+                self.mat_dict[change_side] = new_mat
+                self.canvas.itemconfig(self.parts[i],
+                                       image=new_mat)
 
 
 class FormTwoSection:
@@ -54,7 +74,39 @@ class FormTwoSection:
         self.part_left_bottom = self.canvas.create_rectangle(56, 133, 117, 234)
         self.part_right_bottom = self.canvas.create_rectangle(123, 133, 184, 234)
 
-        self.part_left_top_mat = self.canvas.create_image(86.5, 77, image=self.mirror)
-        self.part_right_top_mat = self.canvas.create_image(153.5, 77, image=self.ldsp)
-        self.part_left_bottom_mat = self.canvas.create_image(86.5, 184, image=self.ldsp)
-        self.part_right_bottom_mat = self.canvas.create_image(153.5, 184, image=self.mirror)
+        self.mat_dict = {
+            'left_top': self.mirror,
+            'right_top': self.ldsp,
+            'left_bottom': self.ldsp,
+            'right_bottom': self.mirror,
+        }
+
+        self.part_left_top_mat = self.canvas.create_image(86.5, 77, image=self.mat_dict['left_top'])
+        self.part_right_top_mat = self.canvas.create_image(153.5, 77, image=self.mat_dict['right_top'])
+        self.part_left_bottom_mat = self.canvas.create_image(86.5, 184, image=self.mat_dict['left_bottom'])
+        self.part_right_bottom_mat = self.canvas.create_image(153.5, 184, image=self.mat_dict['right_bottom'])
+
+        self.parts = {
+            'left_top': self.part_left_top_mat,
+            'right_top': self.part_right_top_mat,
+            'left_bottom': self.part_left_bottom_mat,
+            'right_bottom': self.part_right_bottom_mat,
+        }
+
+        self.canvas.tag_bind(self.part_left_top_mat, '<Button-1>',
+                             lambda event: self.change_material(event, 'left_top'))
+        self.canvas.tag_bind(self.part_right_top_mat, '<Button-1>',
+                             lambda event: self.change_material(event, 'right_top'))
+        self.canvas.tag_bind(self.part_left_bottom_mat, '<Button-1>', 
+                             lambda event: self.change_material(event, 'left_bottom'))
+        self.canvas.tag_bind(self.part_right_bottom_mat, '<Button-1>',
+                             lambda event: self.change_material(event, 'right_bottom'))
+
+    def change_material(self, event, change_side):
+
+        for i in self.parts.keys():
+            if i == change_side:
+                new_mat = self.mirror if self.mat_dict[change_side] == self.ldsp else self.ldsp
+                self.mat_dict[change_side] = new_mat
+                self.canvas.itemconfig(self.parts[i],
+                                       image=new_mat)
